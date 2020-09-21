ac1a3a70b67ca21ff7ad192bc82c41ab57d51cb7
JordanMartinez <jordanalex.martinez@gmail.com>
2020-09-20 19:36:47 -0700

    Update according to Contributors library guidelines (#9)

--- src/Data/HTTP/Method.purs
+++ src/Data/HTTP/Method.purs
@@ -11,6 +11,9 @@ import Prelude
 import Data.Either (Either(..), either)
 import Data.String as Str
 
+-- | The definition of the type is based on HTTP/1.1 with
+-- | [RFC 2518](https://tools.ietf.org/html/rfc2518) and
+-- | [RFC 5789](https://tools.ietf.org/html/rfc5789).
 data Method
   -- HTTP/1.1
   = OPTIONS
@@ -66,26 +69,32 @@ derive instance ordCustomMethod :: Ord CustomMethod
 instance showCustomMethod :: Show CustomMethod where
   show (CustomMethod m) = "(CustomMethod " <> show m <> ")"
 
-fromString :: String -> Either Method CustomMethod
-fromString s =
+-- | Parses a `String` into a `Method` and pass the result into the first
+-- | handler. If the string does not match a known method,
+-- | passes itself into the second handler.
+parse :: (Method -> c) -> (String -> c) -> String -> c
+parse handleMethod handleUnknown s =
   case Str.toUpper s of
-    "OPTIONS" -> Left OPTIONS
-    "GET" -> Left GET
-    "HEAD" -> Left HEAD
-    "POST" -> Left POST
-    "PUT" -> Left PUT
-    "DELETE" -> Left DELETE
-    "TRACE" -> Left TRACE
-    "CONNECT" -> Left CONNECT
-    "PROPFIND" -> Left PROPFIND
-    "PROPPATCH" -> Left PROPPATCH
-    "MKCOL" -> Left MKCOL
-    "COPY" -> Left COPY
-    "MOVE" -> Left MOVE
-    "LOCK" -> Left LOCK
-    "UNLOCK" -> Left UNLOCK
-    "PATCH" -> Left PATCH
-    m -> Right (CustomMethod m)
+    "OPTIONS" -> handleMethod OPTIONS
+    "GET" -> handleMethod GET
+    "HEAD" -> handleMethod HEAD
+    "POST" -> handleMethod POST
+    "PUT" -> handleMethod PUT
+    "DELETE" -> handleMethod DELETE
+    "TRACE" -> handleMethod TRACE
+    "CONNECT" -> handleMethod CONNECT
+    "PROPFIND" -> handleMethod PROPFIND
+    "PROPPATCH" -> handleMethod PROPPATCH
+    "MKCOL" -> handleMethod MKCOL
+    "COPY" -> handleMethod COPY
+    "MOVE" -> handleMethod MOVE
+    "LOCK" -> handleMethod LOCK
+    "UNLOCK" -> handleMethod UNLOCK
+    "PATCH" -> handleMethod PATCH
+    m -> handleUnknown m
+
+fromString :: String -> Either Method CustomMethod
+fromString = parse Left (Right <<< CustomMethod)
 
 print :: Either Method CustomMethod -> String
 print = either show unCustomMethod
